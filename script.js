d1ef6f6262cc72f6861b71e650964abc7a41c204
Daniel_Shulgin <d.shulgin@moonmana.com>
2022-04-22 11:55:35 +0200

    remove redundant

--- script.js
+++ script.js
@@ -10,36 +10,16 @@ const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
 
-// Keep track of current card
 let currentActiveCard = 0;
 
-// Store DOM cards
 const cardsEl = [];
 
-// Store card data
 const cardsData = getCardsData();
 
-// const cardsData = [
-//   {
-//     question: 'What must a variable begin with?',
-//     answer: 'A letter, $ or _'
-//   },
-//   {
-//     question: 'What is a variable?',
-//     answer: 'Container for a piece of data'
-//   },
-//   {
-//     question: 'Example of Case Sensitive Variable',
-//     answer: 'thisIsAVariable'
-//   }
-// ];
-
-// Create all cards
 function createCards() {
   cardsData.forEach((data, index) => createCard(data, index));
 }
 
-// Create a single card in DOM
 function createCard(data, index) {
   const card = document.createElement('div');
   card.classList.add('card');
@@ -65,7 +45,6 @@ function createCard(data, index) {
 
   card.addEventListener('click', () => card.classList.toggle('show-answer'));
 
-  // Add to DOM cards
   cardsEl.push(card);
 
   cardsContainer.appendChild(card);
@@ -73,18 +52,15 @@ function createCard(data, index) {
   updateCurrentText();
 }
 
-// Show number of cards
 function updateCurrentText() {
   currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
-// Get cards from local storage
 function getCardsData() {
   const cards = JSON.parse(localStorage.getItem('cards'));
   return cards === null ? [] : cards;
 }
 
-// Add card to local storage
 function setCardsData(cards) {
   localStorage.setItem('cards', JSON.stringify(cards));
   window.location.reload();
@@ -92,9 +68,6 @@ function setCardsData(cards) {
 
 createCards();
 
-// Event listeners
-
-// Next button
 nextBtn.addEventListener('click', () => {
   cardsEl[currentActiveCard].className = 'card left';
 
@@ -109,7 +82,6 @@ nextBtn.addEventListener('click', () => {
   updateCurrentText();
 });
 
-// Prev button
 prevBtn.addEventListener('click', () => {
   cardsEl[currentActiveCard].className = 'card right';
 
@@ -124,12 +96,10 @@ prevBtn.addEventListener('click', () => {
   updateCurrentText();
 });
 
-// Show add container
 showBtn.addEventListener('click', () => addContainer.classList.add('show'));
-// Hide add container
+
 hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
 
-// Add new card
 addCardBtn.addEventListener('click', () => {
   const question = questionEl.value;
   const answer = answerEl.value;
@@ -149,7 +119,6 @@ addCardBtn.addEventListener('click', () => {
   }
 });
 
-// Clear cards button
 clearBtn.addEventListener('click', () => {
   localStorage.clear();
   cardsContainer.innerHTML = '';
